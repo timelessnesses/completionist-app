208e494adc38fb55cb2a0a07940ff9eb7cbbc224
timelessnesses <mooping@timelessnesses.me>
2026-08-19 15:13:19 +0700

    a

---
+++ android/app/src/main/java/me/timelessnesses/completionist/MainActivity.java
@@ -0,0 +1,15 @@
+package me.timelessnesses.completionist;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onStart() {
+        super.onStart();
+        getBridge().getWebView().getSettings().setUserAgentString(WebSettings.getDefaultUserAgent(this));
+        return;
+    }
+}
